10c0f1ec74a6d738e6d3ebf1eddca7621550ab79
Vedant Tripathii <91128143+Vedantrip@users.noreply.github.com>
2025-07-13 14:31:04 +0530

    Updated main

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Chainfly Rooftop Solar API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://vedantrip.github.io/ChainflySolarApi/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
