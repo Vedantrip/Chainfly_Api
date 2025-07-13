bdbacf97b22fd5f04a761eb90ed460f1c263609f
Vedant Tripathii <91128143+Vedantrip@users.noreply.github.com>
2025-07-13 12:12:08 +0530

    Added newer version of main code for FastAPI

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from routes import feasibility
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Chainfly Rooftop Solar API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(feasibility.router)
+
+# ✅ Add this route
+@app.get("/")
+def home():
+    return {"message": "ChainFly API is live 🚀"}
